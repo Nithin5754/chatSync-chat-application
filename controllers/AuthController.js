a340d3b79e8b7aa237ad1c467badfe8947fd6cff
Nithin Joji0756 <nithinjoji0756@gmail.com>
2024-09-25 02:10:59 +0530

    login and sign up build

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,11 +1,14 @@
 
 import User from "../models/UserModal.js";
 import { createToken, maxAge } from "../utils/tokenCreate.js";
+import bcrypt from "bcrypt";
 
-
-export const signup = async (req, res, next) => {
+export const signup = async (req,res) => {
   try {
-    const { email, password } = req.body;
+    const  {registerData:{ email, password,firstName,lastName }}= req.body;
+
+
+    console.log(req.body,"req.body")
     if (!email || !password) {
       return res.status(200).send("email password required");
     }
@@ -13,6 +16,8 @@ export const signup = async (req, res, next) => {
     const user = await User.create({
       email,
       password,
+      firstName,
+      lastName
     });
 
     res.cookie("jwt", createToken(email, user._id), {
@@ -21,12 +26,14 @@ export const signup = async (req, res, next) => {
       sameSite: "None",
     });
 
+
     return res
       .status(201)
       .json({
         user: {
           id: user._id,
           email: user.email,
+          firstName:user.firstName,
           profileSetup:user.profileSetup,
 
         },
@@ -35,5 +42,44 @@ export const signup = async (req, res, next) => {
     console.log({ error });
 
     return res.status(500).send("internal server error");
+  }
+};
+
+
+export const login = async (req, res) => {
+  console.log("login");
+  
+  const { loginData:{email, password} } = req.body;
+
+  console.log(req.body,"req.body")
+
+  try {
+    const user = await User.findOne({ email });
+
+    console.log(user,"find user")
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    res.cookie("jwt", createToken(email, user._id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+   return res.status(200).json({user: {
+    id: user._id,
+    email: user.email,
+    firstName:user.firstName,
+    profileSetup:user.profileSetup,
+
+  },})
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
