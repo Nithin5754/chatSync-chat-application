e51887e0c659fb0cc47c2800abac09b4770c95a0
Nithin Joji0756 <nithinjoji0756@gmail.com>
2024-10-09 22:14:38 +0530

    update socket sever setup

--- socket.js
+++ socket.js
@@ -1,42 +1,40 @@
-import { disconnect } from "mongoose";
 import { Server as SocketServer } from "socket.io";
 
+function SocketSetUp(server) {
+  const io = new SocketServer(server, {
+    cors: {
+      origin: [process.env.ORIGIN],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+      credentials: true,
+    },
+  });
 
+  const userSocketMap = new Map();
 
-
-
- function SocketSetUp(server) {
-
-
-    const io = new SocketServer(server,{
-      cors:{
-        origin:[process.env.ORIGIN],
-        methods:['GET','POST','PUT','PATCH','DELETE'],
-        credentials:true
+  const disconnectUser = (socket) => {
+    for (const [userId, socketId] of userSocketMap.entries()) {
+      if (socketId === socket.id) {
+        userSocketMap.delete(userId);
+        break;
       }
+    }
+  };
+
+  io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+
+    if (userId) {
+      userSocketMap.set(userId, socket.id);
+      console.log("userConnected" + userId + "with socket id" + socket.id);
+    } else {
+      console.log("User ID not provided during connection");
+    }
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+      disconnectUser(socket);
     });
+  });
+}
 
-
-
-    const userSocketMap=new Map()
-
-    io.on('connection',(socket)=>{
-       const userId=socket.handshake.query.userId; 
-
-       if(userId){
-        userSocketMap.set(userId,socket.id)
-        console.log("userConnected"+userId+"with socket id"+socket.id);
-        
-       }else{
-        console.log("User ID not provided during connection");
-        
-       }
-
-       socket.on("disconnect",()=>disconnect)
-       
-    })
-    
-  }
-
-
-  export default SocketSetUp
+export default SocketSetUp;
